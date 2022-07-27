a37fce13f0fd38a8617c2c72de01dc41d6ffce5b
LOKKEE <kilian_peters@gmx.de>
2022-07-27 23:05:21 +0200

    Changed Google Fonts display to "swap"

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ function Document() {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&family=Roboto:wght@400;500&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;500&display=swap"
           rel="stylesheet"
         />
       </Head>
