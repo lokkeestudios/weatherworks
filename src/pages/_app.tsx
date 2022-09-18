e9b30a559c2418908437ebcbb67d8d68fa78ce26
LOKKEE <kilian_peters@gmx.de>
2022-09-18 15:36:04 +0200

    Set up react-query client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { FavouritesContextProvider } from '../contexts/FavouritesContext';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <FavouritesContextProvider>
-      {/* built-in nextjs feature, thus there is no way around using props spreading in this specific case */
-      /* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </FavouritesContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <FavouritesContextProvider>
+        {/* built-in nextjs feature, thus there is no way around using props spreading in this specific case */
+        /* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </FavouritesContextProvider>
+    </QueryClientProvider>
   );
 }
