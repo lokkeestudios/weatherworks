d513a823366f7044c2b7f8117b1525f11ed1637f
LOKKEE <kilian_peters@gmx.de>
2022-09-18 15:39:54 +0200

    Made SEO slug optional with default fallback

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -5,11 +5,11 @@ import SEO from './SEO';
 
 interface Props {
   title: string;
-  slug: string;
+  slug?: string;
   children: React.ReactNode;
 }
 
-function Layout({ title, slug, children }: Props) {
+function Layout({ title, slug = '', children }: Props) {
   return (
     <>
       <SEO title={title} slug={slug} />
