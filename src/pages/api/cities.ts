f86f9005ba0d81edf001770dbefc2fd9199b6bb5
LOKKEE <kilian_peters@gmx.de>
2022-09-18 15:37:02 +0200

    Set up cities API endpoint with connection to the database

---
+++ src/pages/api/cities.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient();
+
+function errorRes(res: NextApiResponse, status: number, message: string) {
+  return res.status(status).json({ message });
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    return errorRes(res, 405, 'Method not allowed');
+  }
+
+  const { nameFilter, limit } = req.query as Partial<{ [key: string]: string }>;
+
+  if (!nameFilter) {
+    return errorRes(res, 400, 'nameFilter parameter is required');
+  }
+
+  if (!limit) {
+    return errorRes(res, 400, 'limit parameter is required');
+  }
+
+  const limitNum = parseInt(limit, 10);
+
+  if (Number.isNaN(limitNum)) {
+    return errorRes(res, 400, 'limit parameter must be of type number');
+  }
+
+  const cities = await prisma.city.findMany({
+    where: {
+      name: {
+        startsWith: nameFilter,
+      },
+    },
+    take: limitNum,
+  });
+
+  return res.json(cities);
+}
+
+export default handler;
