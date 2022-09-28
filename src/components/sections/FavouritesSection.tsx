aa2a195f777b290be6ea4297b3f80408a898b675
LOKKEE <kilian_peters@gmx.de>
2022-09-28 23:53:00 +0200

    Added temporary no favourites display

--- src/components/sections/FavouritesSection.tsx
+++ src/components/sections/FavouritesSection.tsx
@@ -1,12 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+import { AiFillStar as FavouriteIcon } from 'react-icons/ai';
 import styled from 'styled-components';
 import {
   MAX_FAVOURITES,
   useFavouritesContext,
 } from '../../contexts/FavouritesContext';
 import WeatherCard from '../cards/WeatherCard';
+import CommonStyles from '../styles/CommonStyles';
 import StyledWrapper from '../styles/StyledWrapper';
-import { StyledH2 } from '../styles/TextStyles';
+import {
+  StyledBodyMedium,
+  StyledCaptionLarge,
+  StyledH2,
+} from '../styles/TextStyles';
 
 const StyledSectionWrapper = styled(StyledWrapper)`
   display: grid;
@@ -24,10 +30,25 @@ function FavouritesSection() {
 
   return (
     <StyledSectionWrapper as="section">
-      <StyledH2>{`Favourites (${favourites.favouriteCityIds.length}/${MAX_FAVOURITES})`}</StyledH2>
+      <StyledH2>{`Your favourites (${favourites.favouriteCityIds.length}/${MAX_FAVOURITES})`}</StyledH2>
       <StyledCardsWrapper>
+        {favourites.favouriteCityIds.length === 0 && (
+          <>
+            <FavouriteIcon
+              size={128}
+              color={CommonStyles.colors.text2}
+            />
+            <StyledCaptionLarge>No favourites yet</StyledCaptionLarge>
+            <StyledBodyMedium>
+              Locations you mark as favourite are shown here
+            </StyledBodyMedium>
+          </>
+        )}
         {favourites.favouriteCityIds.map((favouriteCityId) => (
-          <WeatherCard key={favouriteCityId} cityId={favouriteCityId} />
+          <WeatherCard
+            key={favouriteCityId}
+            cityId={favouriteCityId}
+          />
         ))}
       </StyledCardsWrapper>
     </StyledSectionWrapper>
