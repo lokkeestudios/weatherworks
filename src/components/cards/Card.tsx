dcc6e0434449d419d7ec974b5bf2db6c7ae5c5f0
LOKKEE <kilian_peters@gmx.de>
2022-10-11 14:05:12 +0200

    Replaced slate with own neutrals color styles

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -7,13 +7,13 @@ interface Props {
 
 function Card({ title = undefined, children }: Props) {
   return (
-    <div className="rounded-2xl border-0.5 border-slate-50/30 bg-slate-800/50 p-6 backdrop-blur-xl">
+    <div className="rounded-2xl border-0.5 border-neutrals-50/30 bg-neutrals-800/60 p-6 backdrop-blur-xl">
       {title && (
         <>
-          <h2 className="font-display text-2xl font-semibold leading-none">
+          <h2 className="font-display font-semibold leading-none text-2xl">
             {title}
           </h2>
-          <hr className="my-4 border-slate-50/30" />
+          <hr className="my-4 border-neutrals-50/30" />
         </>
       )}
       {children}
