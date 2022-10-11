67ff62d470690e575bb89b0cfb5d2b6c7969d0ff
LOKKEE <kilian_peters@gmx.de>
2022-10-11 14:04:40 +0200

    Fixed the weather card design

--- src/components/cards/WeatherCard.tsx
+++ src/components/cards/WeatherCard.tsx
@@ -84,10 +84,13 @@ function WeatherCard({ geolocation = undefined, cityId = undefined }: Props) {
       {(currentWeatherData) => (
         <Link
           href={`/location/${currentWeatherData.id}`}
-          aria-label="View detailed location page"
+          passHref
         >
-          <div>
-            <p>
+          <a
+            className="flex w-full items-center justify-between rounded-2xl border-0.5 border-neutrals-50/30 bg-neutrals-800/60 px-11 py-6 shadow-lg backdrop-blur-xl transition-all duration-300 focus-visible:-translate-y-1 focus-visible:shadow-2xl hover:-translate-y-1 hover:shadow-2xl"
+            aria-label="View detailed location page"
+          >
+            <p className="font-display font-bold leading-none text-8xl">
               {
                 Math.round(
                   currentWeatherData.main.temp,
@@ -95,19 +98,24 @@ function WeatherCard({ geolocation = undefined, cityId = undefined }: Props) {
               }
               °
             </p>
-            <div>
-              <p>{currentWeatherData.weather[0].description}</p>
-              <h3>
+            <div className="flex flex-col items-center justify-center gap-y-2">
+              <p className="font-medium capitalize leading-none text-neutrals-300 text-sm">
+                {currentWeatherData.weather[0].description}
+              </p>
+              <h3 className="font-display font-bold leading-none text-3xl">
                 {currentWeatherData.name} - {currentWeatherData.sys.country}
               </h3>
             </div>
-            <Image
-              src={`/images/icons/weather/${currentWeatherData.weather[0].icon}.webp`}
-              alt={currentWeatherData.weather[0].description}
-              width={128}
-              height={128}
-            />
-          </div>
+            <div className="w-20 md:w-28 lg:w-36">
+              <Image
+                src={`/images/icons/weather/${currentWeatherData.weather[0].icon}.webp`}
+                alt={currentWeatherData.weather[0].description}
+                width={256}
+                height={256}
+                layout="responsive"
+              />
+            </div>
+          </a>
         </Link>
       )}
     </QueryStateWrapper>
