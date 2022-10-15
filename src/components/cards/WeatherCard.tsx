7718a5db0fd649262b621025f8e785fa96abcb3a
LOKKEE <kilian_peters@gmx.de>
2022-10-16 01:39:06 +0200

    Embedded favourite button into weather cards

--- src/components/cards/WeatherCard.tsx
+++ src/components/cards/WeatherCard.tsx
@@ -1,3 +1,4 @@
+import FavouriteButton from '@/components/forms/FavouriteButton';
 import QueryStateWrapper from '@/components/QueryStateWrapper';
 import { QueryKeys } from '@/proxies';
 import getCurrentWeather from '@/proxies/getCurrentWeather';
@@ -48,37 +49,42 @@ function WeatherCard({
       errorText="Unable to fetch weather data. Try again later"
     >
       {(currentWeatherData) => (
-        <Link
-          href={`/locations/${currentWeatherData.id}`}
-          title="View detailed location page"
-          className="flex w-full items-center justify-between rounded-2xl border-0.5 border-neutrals-50/30 bg-neutrals-800/60 px-8 py-4 shadow-lg backdrop-blur-xl transition-all duration-300 focus-visible:-translate-y-1 focus-visible:scale-[1.005]  focus-visible:shadow-2xl hover:-translate-y-1 hover:scale-[1.005] hover:shadow-2xl lg:px-11 lg:py-6"
-        >
-          <p className="font-display font-bold leading-none text-7xl">
-            {
-              Math.round(
-                currentWeatherData.main.temp,
-              ) /* TODO: make this a utility function or smth */
-            }
-            °
-          </p>
-          <div className="flex flex-col items-center justify-center gap-y-2">
-            <p className="font-medium capitalize leading-none text-neutrals-300 text-sm">
-              {currentWeatherData.weather[0].description}
+        <div className="group relative w-full rounded-2xl border-0.5 border-neutrals-50/30 bg-neutrals-800/60 shadow-lg backdrop-blur-xl transition-all duration-300 focus-visible:-translate-y-1 focus-visible:scale-[1.005] focus-visible:shadow-2xl hover:-translate-y-1 hover:scale-[1.005] hover:shadow-2xl">
+          <div className="absolute top-4 right-8 z-1 opacity-0 transition-opacity duration-300 group-hover:opacity-100 lg:top-6 lg:right-11">
+            <FavouriteButton locationId={currentWeatherData.id} />
+          </div>
+          <Link
+            href={`/locations/${currentWeatherData.id}`}
+            title="View detailed location page"
+            className="flex items-center justify-between py-4 px-8 lg:px-11 lg:py-6"
+          >
+            <p className="font-display font-bold leading-none text-7xl">
+              {
+                Math.round(
+                  currentWeatherData.main.temp,
+                ) /* TODO: make this a utility function or smth */
+              }
+              °
             </p>
-            <h3 className="font-display font-bold leading-none text-2xl">
-              {currentWeatherData.name} - {currentWeatherData.sys.country}
-            </h3>
-          </div>
-          <div className="h-20 w-20 md:h-24 md:w-24 lg:h-32 lg:w-32">
-            <Image
-              src={`/images/icons/weather/${currentWeatherData.weather[0].icon}.webp`}
-              alt={currentWeatherData.weather[0].description}
-              width={256}
-              height={256}
-              layout="responsive"
-            />
-          </div>
-        </Link>
+            <div className="flex flex-col items-center justify-center gap-y-2">
+              <p className="font-medium capitalize leading-none text-neutrals-300 text-sm">
+                {currentWeatherData.weather[0].description}
+              </p>
+              <h3 className="font-display font-bold leading-none text-2xl">
+                {currentWeatherData.name} - {currentWeatherData.sys.country}
+              </h3>
+            </div>
+            <div className="h-20 w-20 md:h-24 md:w-24 lg:h-32 lg:w-32">
+              <Image
+                src={`/images/icons/weather/${currentWeatherData.weather[0].icon}.webp`}
+                alt={currentWeatherData.weather[0].description}
+                width={256}
+                height={256}
+                layout="responsive"
+              />
+            </div>
+          </Link>
+        </div>
       )}
     </QueryStateWrapper>
   );
