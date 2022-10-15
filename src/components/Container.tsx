dc320682dc03748c5044287d283718a177d3e5e7
LOKKEE <kilian_peters@gmx.de>
2022-10-16 01:07:36 +0200

    Made the container a little bit more slim for more white-space on mobile

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 function Container({ children }: Props) {
-  return <div className="mx-auto w-11/12 max-w-6xl">{children}</div>;
+  return <div className="mx-auto w-[90%] max-w-6xl">{children}</div>;
 }
 
 export default Container;
