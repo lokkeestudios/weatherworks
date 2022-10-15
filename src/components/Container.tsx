e70431aa8ebfa3318199a45f1429991018727ed5
LOKKEE <kilian_peters@gmx.de>
2022-10-15 14:16:36 +0200

    Removed the unnecessary feature to dynamically set the html tag of Containers

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,14 +1,11 @@
-import { ReactHTML, ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 interface Props {
-  as?: keyof ReactHTML;
   children: ReactNode;
 }
 
-function Container({ as: ContainerAs = 'div', children }: Props) {
-  return (
-    <ContainerAs className="mx-auto w-11/12 max-w-6xl">{children}</ContainerAs>
-  );
+function Container({ children }: Props) {
+  return <div className="mx-auto w-11/12 max-w-6xl">{children}</div>;
 }
 
 export default Container;
