285534e612dc9ed0239c0777af7ba64a6f4f07cb
LOKKEE <kilian_peters@gmx.de>
2022-10-15 13:02:57 +0200

    Renamed Card to InformationCard to better describe its purpose

--- src/components/cards/InformationCard.tsx
+++ src/components/cards/InformationCard.tsx
@@ -5,12 +5,12 @@ interface Props {
   children: ReactNode;
 }
 
-function Card({ title = undefined, children }: Props) {
+function InformationCard({ title = undefined, children }: Props) {
   return (
     <div className="rounded-2xl border-0.5 border-neutrals-50/30 bg-neutrals-800/60 p-6 backdrop-blur-xl">
       {title && (
         <>
-          <h2 className="font-display font-semibold leading-none text-2xl">
+          <h2 className="font-display font-semibold leading-none text-xl">
             {title}
           </h2>
           <hr className="my-4 border-neutrals-50/30" />
@@ -21,4 +21,4 @@ function Card({ title = undefined, children }: Props) {
   );
 }
 
-export default Card;
+export default InformationCard;
