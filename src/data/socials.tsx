4ab689fd878649a40d927f824ab53910c7138ec2
LOKKEE <kilian_peters@gmx.de>
2022-10-15 13:14:46 +0200

    Added ids for react keys to socials data

--- src/data/socials.tsx
+++ src/data/socials.tsx
@@ -4,18 +4,21 @@ import GitHubIcon from '@/components/icons/GitHubIcon';
 
 const socials = [
   {
+    id: 1,
     title: 'View GitHub profile',
-    icon: GitHubIcon,
+    Icon: GitHubIcon,
     link: 'https://github.lokkeestudios.com',
   },
   {
+    id: 2,
     title: 'Join Discord server',
-    icon: DiscordIcon,
+    Icon: DiscordIcon,
     link: 'https://discord.lokkeestudios.com',
   },
   {
+    id: 3,
     title: 'View source code',
-    icon: CodeIcon,
+    Icon: CodeIcon,
     link: 'https://github.com/lokkeestudios/WeatherWorks',
   },
 ];
