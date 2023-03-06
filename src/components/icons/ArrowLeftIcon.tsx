54d2c63487bfa24499827080b11b3969f34bfa8b
LOKKEE <kilian_peters@gmx.de>
2023-03-06 15:03:47 +0100

    Replaced arrow left with chevron left icon

--- src/components/icons/ArrowLeftIcon.tsx
+++
@@ -1,7 +0,0 @@
-import { HiArrowLeft } from 'react-icons/hi';
-
-function ArrowLeftIcon(props: React.SVGAttributes<SVGElement>) {
-  return <HiArrowLeft {...props} />;
-}
-
-export default ArrowLeftIcon;
---
+++ src/components/icons/ChevronLeftIcon.tsx
@@ -0,0 +1,7 @@
+import { BiChevronLeft } from 'react-icons/bi';
+
+function ChevronLeftIcon(props: React.SVGAttributes<SVGElement>) {
+  return <BiChevronLeft {...props} />;
+}
+
+export default ChevronLeftIcon;
